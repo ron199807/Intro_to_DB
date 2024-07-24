c21da9cd62c6e2d9ba93e880fc04eb7a1e936dfa
ron199807 <ronaldmweema@gmail.com>
2024-07-25 00:47:42 +0200

    task_4

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+USE alx_book_store;
+
+SELECT
+    COLUMN_NAME AS "Column Name",
+    COLUMN_TYPE AS "Column Type",
+    IS_NULLABLE AS "Nullable",
+    COLUMN_DEFAULT AS "Default Value",
+    EXTRA AS "Extra Infor"
+
+FROM
+    INFORMATION_SCHEMA.COLUMNs
+
+WHERE
+    TABLE_SCHEMA = "alx_book_store"
+    AND TABLE_NAME = "Books"
