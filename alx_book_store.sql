89e08a1a2f7c38199875db3bc8512bc2aa378d80
ron199807 <ronaldmweema@gmail.com>
2024-07-24 22:06:51 +0200

    added a book store file

---
+++ alx_book_store.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(125) NOT NULL
+);
+
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR NOT NULL, 
+    adress TEXT
+);
+
+CREATE TABLE Oders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    oder_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    order_details INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL
+    FOREIGN KEY (order_id) REFERENCES Oders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+);
+
+
+
+
