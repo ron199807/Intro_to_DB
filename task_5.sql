95f1d46e6edffb3ed4af049a73c39135e422a204
ron199807 <ronaldmweema@gmail.com>
2024-07-25 01:08:22 +0200

    added a first customer

--- task_5.sql
+++ task_5.sql
@@ -2,4 +2,4 @@ USE alx_book_store;
 
 INSERT INTO customers(customer_id, customer_name, email, address)
 
-VALUES(1, 'Cole Baidoo', 'cbaidoo@sandech.com', '123 Happiness Ave.');
+VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
